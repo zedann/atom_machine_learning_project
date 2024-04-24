194d7a88789fa9c1a8250942fa8df2df2e6809f6
Zedan <zedanmohamedcp@gmail.com>
2024-04-24 20:27:11 +0200

    fix:model Encoder problem

--- model/app.py
+++ model/app.py
@@ -1,7 +1,9 @@
+
 from flask import Flask, request, jsonify
 import joblib
 import pandas as pd
 from flask_cors import CORS
+from transformer import Encoder
 
 
 with open('./logistic_regression.pkl', 'rb') as f:
@@ -22,11 +24,11 @@ def predict():
         json_data = request.get_json()
         data = pd.DataFrame(json_data)
             
-        new_data_transformed = GBR_pipeline.named_steps['preprocessor'].transform(data)
+        new_data_transformed = GBR_pipeline.named_steps['preprocess'].transform(data)
             
-        prediction = GBR_pipeline.named_steps['regressor'].predict(new_data_transformed)
+        prediction = GBR_pipeline.named_steps['classifier'].predict(new_data_transformed)
 
-        return jsonify({'Prediction': prediction.tolist()})
+        return jsonify({'classifier': prediction.tolist()})
     
     except Exception as e:
         return jsonify({"error": str(e)})
