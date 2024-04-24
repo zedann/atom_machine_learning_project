a24c430e7bbec88bf974abb72e18c61be6e573c8
Zedan <zedanmohamedcp@gmail.com>
2024-04-24 19:25:00 +0200

    start backend

---
+++ backend/server.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+const app = require("./app");
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+})
