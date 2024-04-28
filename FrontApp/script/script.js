ac49cb9041426d960e2734af3c7706aa87e3fa45
abdelrahman <abdokhalef29@gmail.com>
2024-04-28 17:52:58 +0300

    last

--- FrontApp/script/script.js
+++ FrontApp/script/script.js
@@ -1,1 +1,9 @@
 console.log("Hello my team!");
+
+document.querySelector(".close").addEventListener("click", function () {
+  document.querySelector(".sidebar-body").style.display = "none";
+});
+
+document.querySelector(".open").addEventListener("click", function () {
+    document.querySelector(".sidebar-body").style.display = "block";
+  });
