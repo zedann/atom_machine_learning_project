d1d1a4c72654bfeecd56db13b8b102253fbb4bb7
Ahmed-Ehab <ahmedehabebrahimahmed@gmail.com>
2024-05-04 21:37:47 +0300

    collect form data

--- FrontApp/script/script.js
+++ FrontApp/script/script.js
@@ -5,5 +5,19 @@ document.querySelector(".close").addEventListener("click", function () {
 });
 
 document.querySelector(".open").addEventListener("click", function () {
-    document.querySelector(".sidebar-body").style.display = "block";
+  document.querySelector(".sidebar-body").style.display = "block";
+});
+
+const patientForm = document.getElementById("patient-form");
+patientForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const patientData = new FormData(patientForm);
+
+  const patientDataObj = {};
+
+  patientData.forEach((value, key) => {
+    patientDataObj[key] = value;
   });
+
+  console.log(patientDataObj);
+});
