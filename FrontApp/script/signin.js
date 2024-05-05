45edb460650dd93043efd740facf3214d6ee976f
abdelrahman <abdokhalef29@gmail.com>
2024-05-05 16:26:20 +0300

    fix-back

--- FrontApp/script/signin.js
+++ FrontApp/script/signin.js
@@ -1,6 +1,6 @@
 const SignIn = document.getElementById("signin-form");
 // SignIn form
-SignIn.addEventListener("submit", (event) => {
+SignIn.addEventListener("submit", async(event) => {
     event.preventDefault();
     const SignInData = new FormData(SignIn);
     const SignInDataObj = {};
@@ -8,5 +8,14 @@ SignIn.addEventListener("submit", (event) => {
     SignInData.forEach((value, key) => {
       SignInDataObj[key] = value;
     });
-    console.log(SignInDataObj);
+    
+    const resToBackEnd = fetch("http://127.0.0.1:3000/api/v1/signin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(SignInDataObj),
+
+    });
+    const dataFromBackend = await resToBackEnd.json();
   })
